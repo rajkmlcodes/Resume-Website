9b01fd71ab5bf0dc8ced8bc6c7c3efa689b60ed8
Raj Kamal <rajkamal4241@gmail.com>
2023-08-25 12:08:05 +0530

    1st Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const printButton = document.getElementById("printBtn");
+const printContainer = document.getElementById("resume");
+
+printBtn.addEventListener("click", () => {
+// Open the print dialog when the button is clicked
+    window.print(printContainer);
+});
